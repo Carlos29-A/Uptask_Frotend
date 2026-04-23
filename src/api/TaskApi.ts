6bf13d5c75b6846a2c52df87b0ee3597d01648ae
Carlos <caguilarvillanueva68@gmail.com>
2026-04-23 16:37:19 -0500

    feat: Add task management functions in TaskApi and integrate delete functionality in TaskCard component; enhance ProjectDetailsView with EditTaskData component for task editing.

--- src/api/TaskApi.ts
+++ src/api/TaskApi.ts
@@ -1,6 +1,6 @@
 import { isAxiosError } from "axios";
 import api from "../lib/axios";
-import { type Project, type TaskFormData } from "../types";
+import { type Project, type Task, type TaskFormData } from "../types";
 
 
 // Crear una tarea
@@ -14,6 +14,69 @@ export async function createTask({ taskData, projectId }: { taskData: TaskFormDa
         if (isAxiosError(error)) {
             throw error.response?.data || {
                 message: "Error al crear la tarea",
+            };
+        }
+        throw error;
+    }
+}
+
+type GetTaskByIdParams = {
+    projectId: Project['_id'];
+    taskId: Task['_id'];
+}
+
+// Obtener una tarea por su id
+export async function getTaskById({ projectId, taskId }: GetTaskByIdParams) {
+    try {
+        const url = `/projects/${projectId}/tasks/${taskId}`;
+        const { data } = await api.get(url);
+        return data;
+    } catch (error) {
+        if (isAxiosError(error)) {
+            throw error.response?.data || {
+                message: "Error al obtener la tarea",
+            };
+        }
+        throw error;
+    }
+}
+
+// Actualizar una tarea
+type UpdateTaskData = {
+    taskData: TaskFormData;
+    projectId: Project['_id'];
+    taskId: Task['_id'];
+}
+export async function updateTask({ taskData, projectId, taskId }: UpdateTaskData) {
+    try {
+        const url = `/projects/${projectId}/tasks/${taskId}`;
+        const { data } = await api.put<string>(url, taskData);
+        return data;
+    } catch (error) {
+        if (isAxiosError(error)) {
+            throw error.response?.data || {
+                message: "Error al actualizar la tarea",
+            };
+        }
+        throw error;
+    }
+}
+
+// Eliminar una tarea
+type DeleteTaskData = {
+    projectId: Project['_id'];
+    taskId: Task['_id'];
+}
+
+export async function deleteTask({ projectId, taskId }: DeleteTaskData) {
+    try {
+        const url = `/projects/${projectId}/tasks/${taskId}`;
+        const { data } = await api.delete<string>(url);
+        return data;
+    } catch (error) {
+        if (isAxiosError(error)) {
+            throw error.response?.data || {
+                message: "Error al eliminar la tarea",
             };
         }
         throw error;
