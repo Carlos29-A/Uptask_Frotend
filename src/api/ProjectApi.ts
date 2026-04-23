4af3b9b039176296d2d1f30d5ecbd44f458445ad
Carlos <caguilarvillanueva68@gmail.com>
2026-04-23 11:31:32 -0500

    feat: Implement getProjects function in ProjectApi for fetching project data and enhance DashboardView to display project list with loading state and error handling.

--- src/api/ProjectApi.ts
+++ src/api/ProjectApi.ts
@@ -1,11 +1,26 @@
 import api from "../lib/axios";
-import type { ProjectFormData } from "../types";
+import { dashboardProjectSchema, type Project, type ProjectFormData } from "../types";
 
 export async function createProject(projectData: ProjectFormData) {
     try {
-
         const { data } = await api.post("/projects", projectData);
         return data;
+    } catch (error) {
+        throw error.response.data || {
+            message: "Error al crear el proyecto",
+        };
+    }
+}
+
+export async function getProjects(): Promise<Project[]> {
+    try {
+        const { data } = await api.get("/projects");
+
+        const response = dashboardProjectSchema.safeParse(data);
+        if (response.success) {
+            return response.data;
+        }
+        return data
     } catch (error) {
         throw error.response.data || {
             message: "Error al crear el proyecto",
