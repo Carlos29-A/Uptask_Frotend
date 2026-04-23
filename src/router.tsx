709d476245c91235d60682556f4f95d8a8c41f43
Carlos <caguilarvillanueva68@gmail.com>
2026-04-23 09:34:00 -0500

    feat: Add CreateProjectView route and enhance DashboardView with project management features and navigation link.

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AppLayout from "./layout/AppLayout";
 import DashboardView from "./views/DashboardView";
+import CreateProjectView from "./views/projects/CreateProjectView";
 
 
 export default function Router() {
@@ -12,6 +13,7 @@ export default function Router() {
                     <Route element={<AppLayout />}>
                         {/* Página principal */}
                         <Route path="/" element={<DashboardView />} index />
+                        <Route path="/projects/create" element={<CreateProjectView />} />
                     </Route>
                 </Routes>
             </BrowserRouter>
--- src/views/projects/CreateProjectView.tsx
+++ src/views/projects/CreateProjectView.tsx
@@ -1,20 +1,18 @@
 import { Link } from "react-router-dom";
 
-
-
-export default function DashboardView() {
+export default function CreateProjectView() {
     return (
         <>
-            <h1 className="text-5xl font-black">Mis proyectos</h1>
+            <h1 className="text-5xl font-black">Crear Proyecto</h1>
             <p className="text-2xl font-light text-gray-500 mt-5">
-                Maneja y administra tus proyectos
+                Llena el siguiente formulario para crear un nuevo proyecto.
             </p>
 
             <Link
                 className="bg-purple-400 hover:bg-purple-500 text-white font-bold py-3 px-10 text-center inline-block mt-5 transition-colors"
-                to="/projects/create"
+                to="/"
             >
-                Nuevo Proyecto
+                Volver a proyectos
             </Link>
         </>
     )
